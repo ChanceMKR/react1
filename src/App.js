0a088b23737895d32bada7ec0ada6a3ed09dac23
ChanceMKR <87632730+ChanceMKR@users.noreply.github.com>
2022-06-27 23:03:25 +0900

    event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+function Header(props){
+  return <header>
+    <h1><a href = "/" onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+
 function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i]
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+      </li>)
   }
   return <nav>
     <ol>
@@ -13,20 +33,7 @@ function Nav(props){
     </ol>
   </nav>
 }
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
 
-}
-function Header(props){
-  return <header>
-    <h1> <a href = "/" onClick={function(event){
-      event.preventDefault();
-    }}>{props.title}</a> </h1>
-  </header>
-}
 function App() {
   const topics = [
     {id:1, title:'html', body:'html is ...'},
@@ -35,10 +42,12 @@ function App() {
   ]
   return (
     <div className="App">
-      <Header title="WEB" onChangeMode={function(){
+      <Header title="WEB" onChangeMode={()=>{
         alert('Header');
       }}></Header>
-      <Nav topics={topics}></Nav>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
